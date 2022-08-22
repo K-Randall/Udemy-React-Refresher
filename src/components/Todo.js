44f02042209728f01b613af77d9399cd40d36c35
Kayla Randall <kayla.randall@willowtreeapps.com>
2022-08-22 14:26:27 -0400

    Handling events
    - added "onClick" attribute to delete button
    - created a function in Todo.js which handles the button click

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,10 +1,17 @@
 function Todo(props) {
+  function deleteHandler() {
+    console.log("clicked here!");
+    console.log(props.text);
+  }
+
   return (
     <div className="card">
       <h2>{props.text}</h2>
       <div className="actions">
         <span>A Span</span>
-        <button className="btn">Delete</button>
+        <button className="btn" onClick={deleteHandler}>
+          Delete
+        </button>
       </div>
     </div>
   );
